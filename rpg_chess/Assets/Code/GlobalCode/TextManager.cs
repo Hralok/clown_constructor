0378bca877c78caf9430e06597344d34d05c49cc
Hralok <hralok@list.ru>
2022-06-06 00:56:05 +0700

    Реализован класс TextManager
    Реализован класс TextManager, который отвечает за работу со строковыми константами в проекте. Функционал класса:
    Метод GetCurrentLanguage возвращает текущий язык приложения в виде переменной типа LanguageEnum.
    Метод SetCurrentLanguage(LanguageEnum newLanguage) устанавливает текущий язык приложения на указанный
    Метод GetTextById(int id) возвращает строковую константу с указанным id на текущем языке приложения
    ____________________
    Также добавлены: Enum LanguageEnum для указания языка, директории Code для файлов программ, Global Code для файлов программ относящимся к проекту в целом и папка Enum для хранения кодовых файлов типа Enum

---
+++ rpg_chess/Assets/Code/GlobalCode/TextManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextManager
+{
+    static LanguageEnum currentLanguage = LanguageEnum.Russian;
+
+    static Dictionary<LanguageEnum, Dictionary<int, string>> allProjectText = new Dictionary<LanguageEnum, Dictionary<int, string>>()
+    {
+        { LanguageEnum.Russian, new Dictionary<int, string>()
+            {
+                {0,  "Главное меню"},
+                {1,  "Настройки"}
+            }
+        },
+
+
+        { LanguageEnum.English, new Dictionary<int, string>()
+            {
+                {0,  "Main menu"},
+                {1,  "Settings"}
+            }
+        }
+    };
+
+    static string GetTextById(int id)
+    {
+        if (allProjectText[currentLanguage].ContainsKey(id))
+        {
+            return allProjectText[currentLanguage][id];
+        }
+        else
+        {
+            throw new System.Exception("Произошла попытка обратиться к несуществуещей строке. Id строки: " + id.ToString() + " Язык: " + currentLanguage.ToString());
+        }
+    }
+
+    static void SetCurrentLanguage(LanguageEnum newLanguage)
+    {
+        if (newLanguage == LanguageEnum.Russian || newLanguage == LanguageEnum.English)
+        {
+            currentLanguage = newLanguage;
+        }
+    }
+
+    static LanguageEnum GetCurrentLanguage()
+    {
+        return currentLanguage;
+    }
+}

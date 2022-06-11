0724f9de18f037d7bcd2f1faae395c0d9b78a060
NeAnastasia <81082567+NeAnastasia@users.noreply.github.com>
2022-06-11 22:34:58 +0700

    Работа со спрайтами
    Подготовлены анимированные спрайты двух существ, создана основа кода для будущей работы с этими спрайтами

---
+++ rpg_chess/Assets/Code/Enums/UnitStateEnum.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum UnitStateEnum
+{
+    Staying,
+    Moving,
+    Attacking,
+    Dying,
+    Dead
+}

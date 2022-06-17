effd3863ae2b496b3759a766413dec6ba6c4acb0
Kamilla Artykova <kamilla20art@gmail.com>
2022-06-18 05:46:04 +0700

    Работа над реализацией предметов
    Обозначены основные поля и методы класса Item.
    Добавлен класс CraftController, регулирующий работу с созданием (крафтом) предметов с использованием других предметов инвентаря.

---
+++ rpg_chess/Assets/Code/Functional Classes/CraftController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftController
+{
+    // Словарь: id предмета - {id предметов для крафта}
+    Dictionary<int, HashSet<int>> craftingCatalog = new Dictionary<int, HashSet<int>>()
+    {
+
+    };
+
+    // Получить id предметов, которые могут быть созданы с помощью переданного предмета
+    public HashSet<int> GetCraftableItemsIds(int itemId)
+    {
+        HashSet<int> items = new HashSet<int>();
+
+        foreach (var craftableItem in craftingCatalog.Keys)
+        {
+            if (craftingCatalog[craftableItem].Contains(itemId))
+            {
+                items.Add(craftableItem);
+            }
+        }
+
+        return items;
+    }
+
+}
--- rpg_chess/Assets/Code/Functional Classes/Item.cs
+++ rpg_chess/Assets/Code/Functional Classes/Item.cs
@@ -4,5 +4,74 @@ using UnityEngine;
 
 public class Item
 {
-    
+    readonly public int itemId;
+    public List<HashSet<Resource>> cost { get; private set; }
+    public ActiveAbility activeAbilitiy { get; private set; }
+    public List<PassiveAbility> passiveAbilities { get; private set; }
+    public int nameId { get; private set; }
+    public int descriptionId { get; private set; }
+
+    // Использовать ли предмет для авто-крафта
+    public bool notCombinable { get; private set; } = false;
+
+    // id предметов, которые могут быть созданы с использованием этого предмета
+    private HashSet<int> craftableItemsIds;
+
+    // Подумать над логикой после расходования (удалить/заменить/...)
+    public bool isItConsumable { get; private set; }
+    private int usageMargin; 
+
+    // Предметы собираются
+
+    public Item(
+        int itemId, int nameId, int descriptionId,
+        bool consumable,
+        List<HashSet<Resource>> cost,
+        ActiveAbility activeAbilitiy, List<PassiveAbility> passiveAbilities
+        )
+    {
+        this.itemId = itemId;
+        this.nameId = nameId;
+        this.isItConsumable = consumable;
+        this.descriptionId = descriptionId;
+        this.cost = cost;
+        this.activeAbilitiy = activeAbilitiy;
+        this.passiveAbilities = passiveAbilities;
+
+        // this.craftableItemsIds = CraftController.GetCraftableItemsIds(itemId);
+    }
+
+    public void DoItNotCombinable(bool notComb)
+    {
+        notCombinable = notComb;
+    }
+
+    // Проверка предметов, которые можно скрафтить из этого
+    public void CheckCraftableItems(List<Item> inventory)
+    {
+
+    }
+
+    // Получать список предметов?? в инвентаре могут быть и ресурсы
+    public void ExecuteAfterAddingToInventoty(List<Item> inventory)
+    {
+        // Объединение с предметами того же типа
+
+        if (!notCombinable)
+        {
+            CheckCraftableItems(inventory);
+        }
+    }
+
+    public void UseItem()
+    {
+        if (isItConsumable)
+        {
+            usageMargin--;
+            if (usageMargin <= 0)
+            {
+                // Действия после израсходования
+            }
+        }
+    }
 }
